525f0f9637f2aba983fb0ead59648e202e6053c4
ThanhPhat <thanhphat0281@gmail.com>
2021-04-13 15:50:43 +0700

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EditHomeComponent } from './edit-home/edit-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:"home", component:HomeComponent}, 
+  {path:"edit-home", component:EditHomeComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EditHomeComponent } from './edit-home/edit-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditHomeComponent
   ],
   imports: [
     BrowserModule,
